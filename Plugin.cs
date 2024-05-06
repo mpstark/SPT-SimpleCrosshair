59470f350f049ee5cd886356869e6f1be5ec224d
Michael P. Starkweather <git@mpstark.com>
2024-05-06 05:46:31 -0500

    initial repo

---
+++ Plugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using SimpleCrosshair.Config;
+
+namespace SimpleCrosshair
+{
+    // the version number here is generated on build and may have a warning if not yet built
+    [BepInPlugin("com.mpstark.SimpleCrosshair", "SimpleCrosshair", BuildInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+        public static ManualLogSource Log => Instance.Logger;
+        public static string Path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        internal void Awake()
+        {
+            Settings.Init(Config);
+            // Config.SettingChanged += (x, y) => PlayerEncumbranceBar.OnSettingChanged();
+
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
